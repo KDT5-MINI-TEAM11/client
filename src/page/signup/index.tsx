0ce3315e6af1647622afad323100e18780d7e7ac
BearHumanS <bye0651@naver.com>
2023-07-28 02:09:36 +0900

    REFATOR: 불필요한 유효성 검사 로직 제거 및 불필요한 상태관리 제거 등등

--- src/page/signup/index.tsx
+++ src/page/signup/index.tsx
@@ -1,39 +1,23 @@
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
 import { Button, Input, Form, Select, Card } from 'antd';
 import { useNavigate } from 'react-router-dom';
-import useInput from '@/page/signup/useInput';
-import { EMAIL_REGEX, PASSWORD_REGEX } from '@/data/constants';
+import { EMAIL_REGEX, PASSWORD_REGEX, POSITIONS } from '@/data/constants';
 
 export default function SingUp() {
-  const [name, onChangeName] = useInput('');
-  const [email, onChangeEmail] = useInput('');
-  const [password, onChangePassword] = useInput('');
-  const [confirmPassword, onChangeConfirmPassword] = useInput('');
-  const [prefixPhoneNumber, setPrefixPhoneNumber] = useState('');
-  const [phoneNumber, onChangePhoneNumber] = useInput('');
-  const [position, setPosition] = useState('');
   const navigate = useNavigate();
-  const [form] = Form.useForm();
   const { Option } = Select;
 
   const onFinish = (values: any) => {
-    console.log('Success:', values);
+    const { prefix, phone, ...otherValues } = values;
+    const phoneNumber = `${prefix}${phone}`;
+
+    const newValues = {
+      ...otherValues,
+      phoneNumber: phoneNumber,
+    };
+    console.log('Success:', newValues);
     navigate('/'); // 회원가입이 성공한 경우 홈으로 이동
   };
-
-  // 이름 유효성 검사
-  const validateName = useCallback((_: any, value: string) => {
-    const NAME_REGEX = /^[가-힣]{2,20}$/;
-
-    if (!value || NAME_REGEX.test(value)) {
-      return Promise.resolve();
-    }
-    if (!NAME_REGEX.test(value) || /[a-zA-Z0-9]/.test(value)) {
-      return Promise.reject(
-        new Error('이름은 한글 음절로 두 글자 이상 입력해야합니다.'),
-      );
-    }
-  }, []);
 
   // 이메일 유효성 검사
   const validateEmail = useCallback((_: any, value: string) => {
@@ -104,144 +88,76 @@ export default function SingUp() {
     return Promise.resolve();
   }, []);
 
-  // 연락처 앞자리 선택 동적으로 생성
-  const selectedNumbers = [0, 1, 6, 7, 8, 9];
-
-  const selectedNumberOptions = selectedNumbers.map((Number) => {
-    const prefix = `01${Number}`;
-    return (
-      <Option key={prefix} value={prefix}>
-        {prefix}
-      </Option>
-    );
-  });
-
-  // 연락처 뒷번호 유효성 검사
-  const validatePhoneNumber = useCallback((_: any, value: string) => {
-    const PHONE_REGEX = /([0-9]{3,4})([0-9]{4})$/;
-
-    if (!value || PHONE_REGEX.test(value)) {
-      return Promise.resolve();
-    }
-
-    return Promise.reject(new Error('올바른 전화번호 형식이 아닙니다.'));
-  }, []);
-
-  // 연락처 앞 번호 (ex. 010 or 011) 선택
-  // select 방식이라 useInput 커스텀 훅 사용하지않고 state로 관리
-  const onChangePrefixPhoneNumber = (value: string) =>
-    setPrefixPhoneNumber(value);
-
-  // 연락처 앞번호와 뒤에 번호를 합쳐주는 함수
-  const getPhoneNumber = () => {
-    return `${prefixPhoneNumber}${phoneNumber}`;
-  };
-
-  const combinedPhoneNumber = getPhoneNumber();
-
   const prefixSelector = (
-    <Form.Item name="prefixPhoneNumber" noStyle>
-      <Select
-        placeholder="-선택-"
-        style={{ width: 85 }}
-        value={prefixPhoneNumber}
-        onChange={onChangePrefixPhoneNumber}
-      >
-        {selectedNumberOptions}
+    <Form.Item name="prefix" noStyle>
+      <Select placeholder="-선택-" style={{ width: 85 }}>
+        <Option value={'010'}>010</Option>
+        <Option value={'011'}>011</Option>
       </Select>
     </Form.Item>
   );
 
   // 직급 선택 동적으로 생성
-  const selectedPositions = [
-    '인턴',
-    '사원',
-    '주임',
-    '대리',
-    '과장',
-    '차장',
-    '부장',
-  ];
-
-  const selectedPositionOptions = selectedPositions.map((position) => {
+  const selectedPositionOptions = Object.keys(POSITIONS).map((key) => {
     return (
-      <Option key={position} value={position}>
-        {position}
+      <Option key={key} value={key}>
+        {POSITIONS[key].label}
       </Option>
     );
   });
 
-  // 직급 선택
-  // select 방식이라 state로 관리
-  const onChangePosition = (value: string) => setPosition(value);
-
-  const handleSignUp = () => {
-    form.validateFields().then(() => {
-      form.submit(); // 입력폼이 모두 유효한 경우에만 onFinish가 호출됨
-    });
-  };
-
   return (
     <Card
       bordered={false}
-      style={{ margin: '0px 20px', maxWidth: 400 }}
+      style={{ margin: '0px 20px', width: 400 }}
       title="회원가입"
     >
       <Form
         layout="vertical"
         name="basic"
         wrapperCol={{ span: 30, offset: 0 }}
-        style={{ maxWidth: 352 }}
+        style={{ maxWidth: 350 }}
         onFinish={onFinish}
         autoComplete="off"
       >
         <Form.Item
           label="이름"
           name="username"
-          rules={[
-            { required: true, message: '이름을 입력해주세요.' },
-            { validator: validateName },
-          ]}
+          rules={[{ required: true, message: '이름을 입력해주세요.' }]}
         >
-          <Input value={name} onChange={onChangeName} allowClear />
+          <Input allowClear />
         </Form.Item>
         <Form.Item
-          label="E-Mail"
-          name="e-mail"
+          label="이메일"
+          name="userEmail"
           rules={[
             { required: true, message: '이메일을 입력해주세요.' },
             { validator: validateEmail },
           ]}
         >
-          <Input
-            placeholder="ex) anyone123@email.com"
-            value={email}
-            onChange={onChangeEmail}
-            allowClear
-          />
+          <Input placeholder="ex) anyone123@email.com" allowClear />
         </Form.Item>
         <Form.Item
           label="비밀번호"
-          name="password"
+          name="userPassword"
           rules={[{ required: true, validator: validatePassword }]}
           hasFeedback
         >
           <Input.Password
-            value={password}
-            onChange={onChangePassword}
+            placeholder="비밀번호는 8자리 이상 16자리 미만입니다."
             allowClear
           />
         </Form.Item>
         <Form.Item
           label="비밀번호 확인"
           name="confirm_password"
-          dependencies={['password']}
+          dependencies={['userPassword']}
           hasFeedback
           rules={[
             { required: true, message: '비밀번호를 입력해주세요' },
             ({ getFieldValue }) => ({
               validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
+                if (!value || getFieldValue('userPassword') === value) {
                   return Promise.resolve();
                 }
                 return Promise.reject(
@@ -251,26 +167,17 @@ export default function SingUp() {
             }),
           ]}
         >
-          <Input.Password
-            value={confirmPassword}
-            onChange={onChangeConfirmPassword}
-            allowClear
-          />
+          <Input.Password allowClear />
         </Form.Item>
         <Form.Item
           label="연락처"
           name="phone"
-          rules={[
-            { required: true, message: '연락처를 입력해주세요.' },
-            { validator: validatePhoneNumber },
-          ]}
+          rules={[{ required: true, message: '연락처를 입력해주세요.' }]}
         >
           <Input
             addonBefore={prefixSelector}
             style={{ width: '100%' }}
-            placeholder="하이픈(-)을 빼고 입력해주세요."
-            value={phoneNumber}
-            onChange={onChangePhoneNumber}
+            placeholder="하이픈(-)없이 12345678"
             allowClear
           />
         </Form.Item>
@@ -281,8 +188,6 @@ export default function SingUp() {
         >
           <Select
             placeholder="-직급-"
-            value={position}
-            onChange={onChangePosition}
             style={{ width: 85, textAlign: 'center' }}
           >
             {selectedPositionOptions}
@@ -293,12 +198,10 @@ export default function SingUp() {
           htmlType="submit"
           size="large"
           style={{ width: '100%' }}
-          onClick={handleSignUp}
         >
           회원가입
         </Button>
       </Form>
-      {combinedPhoneNumber}
     </Card>
   );
 }
