260a84981aab7fc9cfba223e93d824c9634f4114
howooking <junsgk@gmail.com>
2023-07-27 21:20:11 +0900

    feat: 내계정 페이지
    - recoil 설치, 세팅, 튜토리얼
    - 내계정 페이지 dummydata 보여주기

--- src/data/dummyData.ts
+++ src/data/dummyData.ts
@@ -4,7 +4,7 @@ export const DUMMY_USER = {
   user_password: 'qwer1234',
   user_name: 'Kim',
   profile_thumb_url: 'https://picsum.photos/seed/picsum/300/200',
-  position: 'manager',
-  phone_number: '010-1234-5678',
-  remaining_vacation: '15',
+  position: 'LEVEL4',
+  phone_number: '01012345678',
+  remaining_vacation: '12',
 };
--- src/page/myAccount/index.tsx
+++ src/page/myAccount/index.tsx
@@ -1,25 +1,48 @@
 import { DUMMY_USER } from '@/data/dummyData';
-import { Image, Descriptions } from 'antd';
+import { Image, Descriptions, Skeleton } from 'antd';
+import defaultProfile from '@/assets/defaultProfile.png';
+import { POSITIONS } from '@/data/constants';
 
 export default function MyAccount() {
   return (
-    <Descriptions title="내 정보" bordered>
-      <Descriptions.Item label="프로필">
-        <Image height={150} width={200} src={DUMMY_USER.profile_thumb_url} />
-      </Descriptions.Item>
-      <Descriptions.Item label="이름" style={{ height: '100%' }}>
-        {DUMMY_USER.user_name}
-      </Descriptions.Item>
-      <Descriptions.Item label="아이디">
-        {DUMMY_USER.user_email}
-      </Descriptions.Item>
-      <Descriptions.Item label="전화번호">
-        {DUMMY_USER.phone_number}
-      </Descriptions.Item>
-      <Descriptions.Item label="연차">
-        {DUMMY_USER.remaining_vacation}
-      </Descriptions.Item>
-      <Descriptions.Item label="직급">{DUMMY_USER.position}</Descriptions.Item>
-    </Descriptions>
+    <>
+      <Descriptions
+        title="내 정보"
+        bordered
+        column={{ md: 2, sm: 1 }}
+        size="small"
+        labelStyle={{ textAlign: 'center' }}
+      >
+        <Descriptions.Item label="이름">
+          {DUMMY_USER.user_name}
+        </Descriptions.Item>
+        <Descriptions.Item label="이메일">
+          {DUMMY_USER.user_email}
+        </Descriptions.Item>
+        <Descriptions.Item label="전화번호">
+          {DUMMY_USER.phone_number}
+        </Descriptions.Item>
+        <Descriptions.Item label="직급">
+          {POSITIONS[DUMMY_USER.position].label}
+        </Descriptions.Item>
+        <Descriptions.Item label="남은 연차">
+          {DUMMY_USER.remaining_vacation} /{' '}
+          {POSITIONS[DUMMY_USER.position].maxVacation}
+        </Descriptions.Item>
+        <Descriptions.Item label="정보2">$60.00</Descriptions.Item>
+        <Descriptions.Item label="시진">
+          <Image
+            placeholder={
+              <Skeleton.Image active style={{ width: 200, height: 200 }} />
+            }
+            rootClassName="profile_image"
+            width={200}
+            height={200}
+            src={DUMMY_USER.profile_thumb_url}
+            fallback={defaultProfile}
+          />
+        </Descriptions.Item>
+      </Descriptions>
+    </>
   );
 }
